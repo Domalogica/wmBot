9a95a1000be7d4e3c183f131c5e4b98d9ffc0c76
frontjss <frontjss@gmail.com>
2018-04-18 14:29:33 +0300

    new

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def handle_start(message):
         if not message_id:
             menu_list = get_branch_db(message.from_user.id)
             send = bot.send_message(message.from_user.id, "Выберите один из пунктов меню", reply_markup=generator_menu(menu_list))
-            add_message_db(message.chat.id, message.message_id)
+            add_message_db(message.chat.id, send.message_id)
         else:
             bot.delete_message(chat_id=message.chat.id, message_id=message_id)
             del_message_db(message.chat.id)
