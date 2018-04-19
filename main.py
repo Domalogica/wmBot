27f915d879768107528f50a657f9b630a0da4849
frontjss <frontjss@gmail.com>
2018-04-19 11:56:34 +0300

    new

--- main.py
+++ main.py
@@ -83,6 +83,7 @@ def message_handler(message):
         }
         a.param(**add_user)
         result = a.transfer()
+        print(result)
         if result['return'] == "SUCCESSFUL":
             go_back(message.data, message.message.chat.id)
     elif message.data:
