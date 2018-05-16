4b2713da364c7e25c11f5bba2f9251097f1f0706
frontjss <frontjss@gmail.com>
2018-05-16 12:46:01 +0300

    new

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ from settings import *
 import cherrypy
 
 token = "533495913:AAHG-ssiGLwQMCPVBSDG-WVUA8M3aUYzo-0"
-WEBHOOK_HOST = '194.67.204.153'
-WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
-WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
+# WEBHOOK_HOST = '194.67.204.153'
+# WEBHOOK_PORT = 443  # 443, 80, 88 или 8443 (порт должен быть открыт!)
+# WEBHOOK_LISTEN = '0.0.0.0'  # На некоторых серверах придется указывать такой же IP, что и выше
 
-WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
-WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
+# WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Путь к сертификату
+# WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Путь к приватному ключу
 
-WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
-WEBHOOK_URL_PATH = "/%s/" % (token)
+# WEBHOOK_URL_BASE = "https://%s:%s" % (WEBHOOK_HOST, WEBHOOK_PORT)
+# WEBHOOK_URL_PATH = "/%s/" % (token)
 
 
 bot = telebot.TeleBot(token)
@@ -320,24 +320,24 @@ def del_message_db(telegram):
 
 
 
+bot.polling(none_stop=True, interval = 0)
 
 
+# # Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
+# bot.remove_webhook()
 
-# Снимаем вебхук перед повторной установкой (избавляет от некоторых проблем)
-bot.remove_webhook()
+#  # Ставим заново вебхук
+# bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
+#                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
- # Ставим заново вебхук
-bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
-                certificate=open(WEBHOOK_SSL_CERT, 'r'))
+# # Указываем настройки сервера CherryPy
+# cherrypy.config.update({
+#     'server.socket_host': WEBHOOK_LISTEN,
+#     'server.socket_port': WEBHOOK_PORT,
+#     'server.ssl_module': 'builtin',
+#     'server.ssl_certificate': WEBHOOK_SSL_CERT,
+#     'server.ssl_private_key': WEBHOOK_SSL_PRIV
+# })
 
-# Указываем настройки сервера CherryPy
-cherrypy.config.update({
-    'server.socket_host': WEBHOOK_LISTEN,
-    'server.socket_port': WEBHOOK_PORT,
-    'server.ssl_module': 'builtin',
-    'server.ssl_certificate': WEBHOOK_SSL_CERT,
-    'server.ssl_private_key': WEBHOOK_SSL_PRIV
-})
-
- # Собственно, запуск!
-cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
+#  # Собственно, запуск!
+# cherrypy.quickstart(WebhookServer(), WEBHOOK_URL_PATH, {'/': {}})
