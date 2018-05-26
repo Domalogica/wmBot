e48af6c9a0e2050abd20519059a7e8875771492f
frontjss <frontjss@gmail.com>
2018-05-26 13:33:54 +0300

    new

--- db.py
+++ db.py
@@ -14,7 +14,7 @@ class MysqlPython(object):
     __session    = None
     __connection = None
 
-    def __init__(self, host='194.67.217.180', user='root', password='', database='', charset='utf8'):
+    def __init__(self, host='194.67.204.153', user='root', password='', database='', charset='utf8'):
         self.__host     = host
         self.__user     = user
         self.__password = password
